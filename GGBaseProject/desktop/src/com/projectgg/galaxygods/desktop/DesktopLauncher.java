b984ef554c350bb3dcf79e992df15c3c4c33f6d9
Fabian Schneekloth <fabian.schneekloth@web.de>
2016-04-24 13:18:58 +0200

    bla

---
+++ GGBaseProject/desktop/src/com/projectgg/galaxygods/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.projectgg.galaxygods.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.projectgg.galaxygods.GalaxyGods;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GalaxyGods(), config);
+	}
+}
