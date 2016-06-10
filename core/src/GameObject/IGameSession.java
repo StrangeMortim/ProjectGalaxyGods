38b12bcf2d76643d9e55f3bf34504f7f7eda3d28
brennben <benjamin.brennecke@yahoo.com>
2016-06-10 20:52:30 +0200

    GameSession+Interface

---
+++ core/src/GameObject/IGameSession.java
@@ -0,0 +1,14 @@
+package GameObject;
+
+/**
+ * Created by benja_000 on 10.06.2016.
+ */
+public interface IGameSession {
+   // public void addTeam(Team t);
+    //public void removeTeam(Team t);
+    //public void sendMessage(Message m);
+    //public boolean playerJoin(Account a, Player p, Team t){};
+    //public void playerLeave(Player p);
+    public boolean save();
+    public boolean finish();
+}
