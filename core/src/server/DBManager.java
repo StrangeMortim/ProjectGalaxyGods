ec13a314c927847ed64aeef789d878a66cf4b7b9
brennben <benjamin.brennecke@yahoo.com>
2016-06-18 17:37:34 +0200

    DBManager kann noch nicht mit dem test package getestet werden, weil gradle Junit nicht will, egal was ich tue. Ich werde das package jetzt aus dem Build entfernen und versuchen da von Außen dran zu gehen

--- core/src/server/DBManager.java
+++ core/src/server/DBManager.java
@@ -7,26 +7,112 @@ import java.io.*;
 import java.sql.*;
 
 /**
- *
+ * Diese Klasse realisiert das Speichern,Erstellen sowie Laden von GameSession- und Account-Objekten.
+ * Sie nutzt dazu eine Derby Datenbank mit der sie ueber JDBC kommuniziert.
  */
 public class DBManager
 {
+    //Verbindungsadresse der Datenbank.
+    final static String DB_URL = "jdbc:derby:DerbyDB;create=true";
+
+    /**
+     * Dieser Konstruktor erstellt, bei Bedarf, fehlende Bereiche der Datenbank.
+     */
+    public DBManager()
+    {
+        try
+        {
+            Connection conn = DriverManager.getConnection(DB_URL);
+            try{buildGameSessionTable(conn);
+            }catch(Exception e){System.out.println("GameSession-Table ist vorhanden.");}
+            try{buildAccountTable(conn);
+            }catch(Exception e){System.out.println("Account-Table ist vorhanden.");}
+            conn.close();
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+
     /**
      * Laedt die GameSession mit spezifischem Namen.
      * @param sessionName Name der GameSession
      * @return GameSession mit dem jeweiligen Namen, sonst null.
      */
-    public GameSession loadSession(String sessionName){
-        return null;
+    public static GameSession loadSession(String sessionName){
+        GameSession session=null;
+        try {
+            Connection conn = DriverManager.getConnection(DB_URL);
+            PreparedStatement sta = conn.prepareStatement(
+                    "SELECT session FROM GameSessions WHERE id = '"+sessionName+"'");
+            ResultSet res = sta.executeQuery();
+            while (res.next()) {
+                byte[] st =  res.getBytes(1);
+                ByteArrayInputStream baip = new ByteArrayInputStream(st);
+                ObjectInputStream ois = new ObjectInputStream(baip);
+                session = (GameSession) ois.readObject();
+            }
+            conn.close();
+            sta.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+        return session;
     }
 
     /**
      *Speichert die uebergebene GameSession.
-     * @param session zu speichernde GameSession
+     * @param gs zu speichernde GameSession
      * @return true, falls Speichern erfolgreich, sonst false.
      */
-    public boolean saveSession(GameSession session){
-        return false;
+    public boolean saveSession(GameSession gs){
+
+  if(getSessionList().matches("(.*)"+gs.getName()+"(.*)")){
+      try{
+          Connection conn = DriverManager.getConnection(DB_URL);
+          String update="UPDATE GameSessions SET session = ? WHERE id= ?";
+          PreparedStatement pstmt = conn.prepareStatement(update);
+          ByteArrayOutputStream baos = new ByteArrayOutputStream();
+          ObjectOutputStream oos = new ObjectOutputStream(baos);
+          oos.writeObject(gs);
+          byte[] gBytes = baos.toByteArray();
+          ByteArrayInputStream bais = new ByteArrayInputStream(gBytes);
+          pstmt.setBinaryStream(1, bais, gBytes.length);
+          pstmt.setString(2,gs.getName());
+          pstmt.executeUpdate();
+          pstmt.close();
+          conn.close();
+          return true;
+      }catch(Exception e){
+          e.printStackTrace();System.out.println(e.getMessage());
+        System.out.println("Beim Update der GameSession ist etwas fehlgeschlagen!");
+          return false;
+      }
+  }
+        try {
+            Connection conn = DriverManager.getConnection(DB_URL);
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ObjectOutputStream oos = new ObjectOutputStream(baos);
+            oos.writeObject(gs);
+            byte[] gBytes = baos.toByteArray();
+            PreparedStatement pstmt = conn
+                    .prepareStatement("INSERT INTO GameSessions (id, session) VALUES(?,?)");
+            ByteArrayInputStream bais = new ByteArrayInputStream(gBytes);
+            pstmt.setBinaryStream(2, bais, gBytes.length);
+            pstmt.setString(1, gs.getName());
+            pstmt.executeUpdate();
+            pstmt.close();
+            conn.close();
+            return true;
+        }catch(Exception e){
+            e.printStackTrace();System.out.println(e.getMessage());
+            System.out.println("Beim Speichern der GameSession ist etwas fehlgeschlagen!");
+            return false;
+        }
     }
 
     /**
@@ -36,8 +122,18 @@ public class DBManager
      * @param password Passwort vom Account
      * @return true, wenn Registration geklappt hat, sonst false
      */
-    public boolean registerAccount(String name, String password){
-        return false;
+    public static boolean registerAccount(String name, String password){
+try {
+    Connection conn = DriverManager.getConnection(DB_URL);
+    PreparedStatement pstmt = conn
+            .prepareStatement("INSERT INTO Accounts (id, password) VALUES(?,?)");
+    pstmt.setString(1,name);
+    pstmt.setString(2,password);
+    pstmt.executeUpdate();
+    pstmt.close();
+    conn.close();
+    return true;
+}catch(Exception e){return false;}
     }
 
     /**
@@ -47,164 +143,145 @@ public class DBManager
      * @param password Passwort des Accounts
      * @return true, wenn Pruefung erfolgreich, sonst false.
      */
-    public boolean checkAccount(String name, String password){
+    public static boolean checkAccount(String name, String password){
+        if(getAccountList().matches("(.*)"+name+"(.*)")) {
+try {
+    Connection conn = DriverManager.getConnection(DB_URL);
+    PreparedStatement sta = conn.prepareStatement(
+            "SELECT password FROM Accounts WHERE id = '" + name + "'");
+    ResultSet res = sta.executeQuery();
+    String pw="";
+    while (res.next()) {
+        pw = res.getString(1);
+    }
+    sta.close();
+    res.close();
+    conn.close();
+    if(pw.equals(password)){
+        return true;}
+}catch(Exception e){return false;}}
         return false;
     }
 
     /**
      * Gibt alle GameSession-Namen zurueck, die auf dem Server gespeichert sind.
-     *
      * @return Namen der GameSession-Objekte
      */
-    public String getSessionList(){
-        return "";
-    }
-
-    public DBManager()
-    {
-
-        try
-        {
-            // Create a named constant for the URL.
-            // NOTE: This value is specific for Java DB.
-            final String DB_URL = "jdbc:derby:DerbyDB;create=true";
-
-            // Create a connection to the database.
-            Connection conn =
-                    DriverManager.getConnection(DB_URL);
-
-            // If the DB already exists, drop the tables.
-            dropTables(conn);
-
-
-            buildGameSessionTable(conn);
-
-
-            // Close the connection.
+    public static String getSessionList(){
+        String list="";
+        try {
+            Connection conn = DriverManager.getConnection(DB_URL);
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("SELECT id FROM GameSessions");
+            while (rs.next()) {
+                String s = rs.getString(1);
+                list=list+s+";";
+            }
+            st.close();
+            rs.close();
             conn.close();
-        } catch (Exception e)
-        {
+        }catch(Exception e){
+            e.printStackTrace();
             System.out.println(e.getMessage());
         }
-
+        return list;
     }
 
     /**
-     * The dropTables method drops any existing
-     * in case the database already exists.
+     * Gibt alle Accountnamen zurueck, die auf dem Server gespeichert sind.
+     * @return Namen der Accounts
      */
-    public static void dropTables(Connection conn)
-    {
-        System.out.println("Checking for existing tables.");
+    public static String getAccountList(){
+        String list="";
+        try {
+            Connection conn = DriverManager.getConnection(DB_URL);
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("SELECT id FROM ACCOUNTS");
+            while (rs.next()) {
+                String s = rs.getString(1);
+                list=list+s+";";
+            }
+            rs.close();
+            st.close();
+            conn.close();
+        }catch(Exception e){
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+        return list;
+    }
 
+    /**
+     * Loescht alle Datenbankeintraege der GameSessions und Accounts
+     */
+    public static void dropTables()
+    {
         try
         {
-            // Get a Statement object.
+            Connection conn = DriverManager.getConnection(DB_URL);
             Statement stmt = conn.createStatement();
-
-            try
-            {
-                stmt.execute("DROP TABLE GameSessions");
-                System.out.println("GameSessions table dropped.");
-            } catch (SQLException ex)
-            {
-                // No need to report an error.
-                // The table simply did not exist.
-            }
-
-
-
+            stmt.execute("DROP TABLE GameSessions");
+            stmt.execute("DROP TABLE Accounts");
+            System.out.println("GameSessions und Accounts wurden geloescht.");
+            stmt.close();
+            conn.close();
         } catch (SQLException ex)
         {
-            System.out.println("ERROR: " + ex.getMessage());
+            System.out.println("Fehler: " + ex.getMessage());
             ex.printStackTrace();
         }
     }
 
+    /**
+     * Loescht die GameSession mit eingegebenen Namen.
+     * @param name Name des GameSession-Objektes
+     * @return true, falls es geklappt hat, sonst false
+     */
+    public static Boolean deleteGameSession(String name){
+        try{
+            Connection conn = DriverManager.getConnection(DB_URL);
+            PreparedStatement pstmt = conn
+                    .prepareStatement("DELETE FROM GameSessions WHERE id = ?");
+            pstmt.setString(1, name);
+            pstmt.executeUpdate();
+            pstmt.close();
+            conn.close();
+            System.out.println("GameSession mit Namen: '"+name+"' wurde geloescht!");
+            return true;
+        }catch(Exception e){
+            System.out.println("Die GameSession mit Namen: '"+name+"' existiert nicht!");
+            return false;
+        }
+    }
 
-
-
-
-
+    /**
+     * Erstellt die Tabelle in der die GameSession-Objekte mit ihren spezifischen Namen
+     * gespeichert werden sollen.
+     * @param conn Verbindung zu der Datenbank
+     */
     public static void buildGameSessionTable(Connection conn)
     {
         try
         {
-            // Get a Statement object.
             Statement stmt = conn.createStatement();
             stmt.executeUpdate("CREATE TABLE GameSessions (id VARCHAR(100), session Blob)");
             conn.commit();
-
-
-            try {
-
-                GameSession gs = new GameSession();
-                gs.setRound(2);
-                gs.setHasStarted(false);
-                gs.setMaxPlayersPerTeam(2);
-                Market m = new Market();
-                m.setIron(1000);
-                m.setWood(2000);
-                gs.setMarket(m);
-
-                ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                ObjectOutputStream oos = new ObjectOutputStream(baos);
-                oos.writeObject(gs);
-                byte[] employeeAsBytes = baos.toByteArray();
-                PreparedStatement pstmt = conn
-                        .prepareStatement("INSERT INTO GameSessions (id, session) VALUES(?,?)");
-                ByteArrayInputStream bais = new ByteArrayInputStream(employeeAsBytes);
-                pstmt.setBinaryStream(2, bais, employeeAsBytes.length);
-                pstmt.setString(1,"jo");
-                pstmt.executeUpdate();
-                pstmt.close();
-
-                System.out.println("GameSessionTable created.");
-
-            }catch(Exception e){
-                System.out.println("ERROR1: " + e.getMessage());
-            }
-
-
-
-
-           try{
-               Statement stmt2 = conn.createStatement();
-               ResultSet rs = stmt2.executeQuery("SELECT session FROM GameSessions");
-               while (rs.next()) {
-                   byte[] st =  rs.getBytes(1);
-
-                   ByteArrayInputStream baip = new ByteArrayInputStream(st);
-
-                   ObjectInputStream ois = new ObjectInputStream(baip);
-
-                   GameSession emp = (GameSession) ois.readObject();
-
-                   System.out.println(emp.getMarket().woodPrice());
-               }
-               stmt2.close();
-
-
-
-               Statement test = conn.createStatement();
-               rs=test.executeQuery("SELECT id From GameSessions");
-               while (rs.next()) {
-                   String emp = rs.getString(1);
-
-                   System.out.println(emp);
-               }
-               test.close();
-               rs.close();
-
-               conn.close();
-            }catch(Exception e){
-               System.out.println("ERROR2: " + e.getMessage());
-           }
-
-        } catch (SQLException ex)
-        {
-            System.out.println("ERROR3: " + ex.getMessage());
-        }
+            System.out.println("GameSession-Table wurde erstellt.");
+        } catch (Exception ex) {}
     }
 
+    /**
+     * Erstellt die Tabelle in der die Account-Daten gespeichert werden.
+     * @param conn
+     */
+    public static void buildAccountTable(Connection conn) {
+        try{
+            Statement stmt = conn.createStatement();
+            stmt.executeUpdate("CREATE TABLE Accounts (id VARCHAR(100), password VARCHAR(100))");
+            conn.commit();
+            System.out.println("Account-Table wurde erstellt.");
+        }catch(Exception e){}
+    }
 }
+
+
--- core/src/tests/DBManagerTest.java
+++
@@ -1,9 +0,0 @@
-package tests;
-
-/**
- * Created by benja_000 on 14.06.2016.
- */
-public class DBManagerTest {
-
-
-}
