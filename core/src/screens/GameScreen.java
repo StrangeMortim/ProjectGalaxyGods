e5387bfd44e5fa3257b8131a21edaeefebea9aa4
Fabian Schneekloth <fabian.schneekloth@web.de>
2016-07-04 13:49:50 +0200

    gamescreen da mit default attributen

--- core/src/screens/GameScreen.java
+++ core/src/screens/GameScreen.java
@@ -1,7 +1,84 @@
 package screens;
 
+import GameObject.GameSession;
+import GameObject.Unit;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+
 /**
  * Created by benja_000 on 03.07.2016.
  */
-public class GameScreen {
+public class GameScreen implements Screen{
+
+    private Game game;
+    private GameSession session;
+    private Unit selected;
+
+    private Skin skin;
+    private Sprite field;
+    private SpriteBatch batch;
+
+
+    /**
+     * Called when this screen becomes the current screen for a {@link Game}.
+     */
+    @Override
+    public void show() {
+
+    }
+
+    /**
+     * Called when the screen should render itself.
+     *
+     * @param delta The time in seconds since the last render.
+     */
+    @Override
+    public void render(float delta) {
+
+    }
+
+    /**
+     * @param width
+     * @param height
+     * @see //ApplicationListener#resize(int, int)
+     */
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    /**
+     * @see //ApplicationListener#pause()
+     */
+    @Override
+    public void pause() {
+
+    }
+
+    /**
+     * @see //ApplicationListener#resume()
+     */
+    @Override
+    public void resume() {
+
+    }
+
+    /**
+     * Called when this screen is no longer the current screen for a {@link Game}.
+     */
+    @Override
+    public void hide() {
+
+    }
+
+    /**
+     * Called when this screen should release all resources.
+     */
+    @Override
+    public void dispose() {
+
+    }
 }
